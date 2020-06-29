3b800a9618d18be178fdd509438ca6f425297d76
saeed poorjandaghi <saeed.poorjandaghi@gmail.com>
2020-06-29 15:06:41 +0430

    initial commit

--- sysTick/main.c
+++ sysTick/main.c
@@ -1,12 +1,10 @@
 /*
- * uart-console.c
+ * sysTick.c
  *
  * Author: 
  * Saeed Poorjandaghi
  *
  * Pinout: 
- * TX: PA9
- * RX: PA10
  * LED Blue : PB6
  * LED Green: PB7
  * USER button: PA0
@@ -15,17 +13,15 @@
  * 	HSI RC 16MHz with AHB prescaler 1, cortex system timer divider 1, APB1 and APB2 prescaler 1 --> 16MHz
  *
  * Purpose:   
- * to have serial interaction console (9600, 8 bit data size, no parity and handshake off)
+ * to use percise system delau using sysTick
  *
- * Note : 
- * Compiler version 6 or higher find redifinition of struct __FILE as error.
- * Use standard serial console terminal like Tera Term.
  *
  * Configuration:
- * 1. Redefine low-level library functions to enable
- *    direct use of high-level library functions.
- *
- * 
+ * 1. Set the SysTick reloud value register based on desired delay @16MHz clock.
+ * 2. Clear the SysTick current value register.
+ * 3. (optional) set the calibration register if needed.    
+ * 4. Program control and status register. 
+ * 5. Check control and status register for count flag to be set. 
 .
 
  */
@@ -34,10 +30,8 @@
 #include <stdio.h>
  
  /* Function declarations */
- void usart1_Init(void);
- int usart1_write(char ch);
- char usart1_read(void);
  void gpio_init(void);
+ void sys_tick_init(void);
  void led_blue(int status);
  void led_blue_toggle(void);
  void led_green(int status);
@@ -46,70 +40,23 @@
  void delay_ms(int delay);
  
  int main(void)
- {
-	 char c;
-	 char str[100];
-	 
+ {	 
 	 set_sysclk_to_hsi();
 	 gpio_init();
-	 usart1_Init();
-	 
-	 printf(" ");		// dummy send
-	 printf("Hello stranger\n");
-	 fprintf(stdout,"This is stdout\r\n");
-	 fprintf(stderr,"and this is stderr\r\n");
-	 
+	 sys_tick_init();
+ 
 	 while(1)
 	 {
-			printf("Which LED do you want me to toggle? (type B for blue or G for green)\r\n");
-		  scanf("%c",&c);
-		  printf("You choose %c \r\n",c);
-		  if(c == 'B' || c == 'b')
+		  // check if count flag is set
+			if((SysTick->CTRL & 0x00010000) == 0x00010000)
 			{
-				led_blue_toggle();
+				 led_blue_toggle();
+				 led_green_toggle();
 			}
-			else if(c == 'G' || c == 'g')
-			{
-				led_green_toggle();
-			}
-		  printf("Now enter your name: ");
-		  gets(str);
-		  printf("You are doing great ");
-		  puts(str);
-		  printf("\r\n");
 	 } 
  }
  
  
- void usart1_Init(void)
-{
-	RCC->APB2ENR |= 0x00004000;		// enable clock for APB2ENR bus (bit 14) --> 	USART1
-	RCC->AHBENR |= 0x00000001;		// enable clock for AHBENR bus (bit 1) --> GPIOA 
-	
-	GPIOA->AFR[1] |= 0x00000770;	// alternate selection for PA9 and PA10 as usart
-	GPIOA->MODER |= 0x00280000; 	// alternate function mode for PA9 and PA10 
-	
-  USART1->BRR = 0x0683;									// baudrate 9600 @ 16Mhz	
-	USART1->CR1 = 0x0000200C;							// enable UE and TE/RE 
-	
-}
- 
-int usart1_write(char ch)
-{
-	//while(!(USART1->SR & 0x0080)){}	//wait while TX buffer is empty
-	USART1->DR = ch & 0xFF;
-  while(!(USART1->SR & 0x0040)){}		// wait until transmission is completed
-		
-	return ch;	
-}
-
-char usart1_read(void)
-{
-	while(!((USART1->SR & 0x00000020) == 0x00000020)){}	// wait until received data is ready to be read
-		
-	return (char)USART1->DR;		
-}
-
  void gpio_init(void)
  {
 	 RCC->AHBENR |= 0x00000002;			// IO port B clock enabled (bit 1) --> All GPIOs are connected to AHB bus, so the relevant register is RCC_AHBENR 
@@ -117,6 +64,13 @@ char usart1_read(void)
 	 
 	 RCC->AHBENR |= 0x00000001;										// IO port A clock enabled (bit 0) --> All GPIOs are connected to AHB bus, so the relevant register is RCC_AHBENR 
 	 GPIOA->MODER &= (unsigned int)~0x00000003;		// set PA0 as input (0b00)	
+ }
+ 
+ void sys_tick_init(void)
+ {
+	 SysTick->LOAD = 16000000-1;		// @16MHz to generate 1second delay
+	 SysTick->VAL = 0;
+	 SysTick->CTRL = 0x00000005;		// clock source: processor clock, counter enabled
  }
  
  void led_blue(int status)
@@ -191,32 +145,4 @@ void delay_ms(int delay)
 	}
 }
 
-// Check if compiler version 4/5 is selected in target option
-#if   defined ( __CC_ARM )		
-struct __FILE
-{
-	int handle;
-};
-#endif
 
-
-FILE __stdin  = {0};
-FILE __stdout = {1};
-FILE __stderr = {2};
-
-int fgetc(FILE *f)
-{
-	char c;
-	c = usart1_read();
-	if(c == '\r')				// enter
-	{
-		usart1_write(c);
-		c = '\n';
-	}
-	usart1_write(c);
-}
-
-int fputc(int c, FILE *f)
-{
-	return usart1_write((char)c);
-}
