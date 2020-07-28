bdb4f3d76eb2902508632d44fa24e88882a1d91a
saeed poorjandaghi <saeed.poorjandaghi@gmail.com>
2020-07-28 14:51:57 +0430

    initial commit

---
+++ adc/Objects/startup_stm32l152xc.d
@@ -0,0 +1,1 @@
+.\objects\startup_stm32l152xc.o: RTE\Device\STM32L152RC\startup_stm32l152xc.s
---
+++ adc/main.c
@@ -0,0 +1,212 @@
+/*
+ * adc.c
+ *
+ * Author: 
+ * Saeed Poorjandaghi
+ *
+ * Pinout: 
+ * TX					: PA9
+ * RX					: PA10
+ * LED Blue 	: PB6
+ * LED Green	: PB7
+ * USER button: PA0
+ * ADC				: PA4
+ *
+ * Clock:
+ * 	HSI RC 16MHz with AHB prescaler 1, cortex system timer divider 1, APB1 and APB2 prescaler 1 --> 16MHz
+ *
+ * Purpose:   
+ * read analog channel PA4 and send voltage value to serial port.
+ *
+ *
+ * Configuration:
+ * init:
+ * 1. enable clock for ADC1 in APB2ENR register
+ * 2. enable clock for PORTA in AHBENR
+ * 3. set PA4 as analog pin
+ * 4. disable adc
+ * 5. set regular sequence registers.
+ * 6. enable ADC
+ * 
+ * conversion:
+ * 1. check ADONS bit in ADC_CR register to check if ADC is ready. 
+ * 2. start convertion by setting SWSTART bit ADC_CR2 register.
+ * 3. wait until EOC bit in ADC_CR is set (conversion completed)
+ * 4. read ADC_DR for conversion result
+ */
+ 
+#include "stm32l1xx.h"                  // Device header
+ 
+ /* Function declarations */
+ void adc_init(void);
+ unsigned int adc_conversion(void);
+ void usart1_Init(void);
+ void usart1_write(char ch);
+ void usart1_send_string(char * str);
+ char usart1_read(void);
+ void gpio_init(void);
+ void led_blue(int status);
+ void led_green(int status);
+ void set_sysclk_to_hsi(void);
+ void delay_ms(int delay);
+ 
+ int main(void)
+ {
+	 unsigned int adc_result = 0;
+	 char str[40];
+	 set_sysclk_to_hsi();
+	 gpio_init();
+	 usart1_Init();
+	 adc_init();
+	 
+	 while(1)
+	 {
+			adc_result = adc_conversion();
+		  sprintf(str,"adc value = %1.2f\r\n",(float)adc_result*3.00/4092);
+		  usart1_send_string(str);
+		  delay_ms(1000);
+	 } 
+ }
+ 
+ unsigned int adc_conversion(void)
+ {
+	  unsigned int value = 0;
+	  while(!((ADC1->SR & 0x00000040) == 0x00000040)){}								// wait until the ADC is ready to covert
+	  ADC1->CR2 |= 0x40000000;																				// starts conversion of regular channels
+	  while(!((ADC1->SR & 0x00000002) == 0x00000002)){}								// wait until regular channel end of conversion
+			
+		value = ADC1->DR;
+				
+		return value;	
+ }
+ 
+ void adc_init(void)
+ {
+		RCC->APB2ENR |= 0x00000200;									// ADC1 interface clock enabled
+	  RCC->AHBENR  |= 0x00000001;									// enable clock for AHBENR bus (bit 1) --> GPIOA 
+	 
+	  GPIOA->MODER |= 0x00000300;									// PA4 as analog mode
+	  
+	  ADC1->CR2 	&= 0x00000000;									// disable ADC 
+	  ADC1->SQR5 	|= 0x00000004;									// 1st conversion in regular sequence for ADC channel 4
+	  ADC1->CR2 	|= 0x00000001;									// enable ADC  
+ }
+ 
+ void USART1_IRQHandler(void)
+ {
+	 char c;
+	 if(USART1->SR & 0x00000020)			// check if received data is ready to be read.
+	 {
+			c =  (char)USART1->DR;
+		  usart1_send_string("test");
+		  //usart1_write(c);							// echo
+	 }
+ }
+ 
+ void usart1_Init(void)
+{
+	RCC->APB2ENR |= 0x00004000;		// enable clock for APB2ENR bus (bit 14) --> 	USART1
+	RCC->AHBENR |= 0x00000001;		// enable clock for AHBENR bus (bit 1) --> GPIOA 
+	
+	GPIOA->AFR[1] |= 0x00000770;	// alternate selection for PA9 and PA10 as usart
+	GPIOA->MODER |= 0x00280000; 	// alternate function mode for PA9 and PA10 
+	
+  USART1->BRR = 0x0683;									// baudrate 9600 @ 16Mhz	
+	USART1->CR1 = 0x0000200C;							// enable UE and TE/RE 
+	
+	__disable_irq();											// disable all interrupts
+	USART1->CR1 |= 0x00000020;						// an USART interrupt is generated whenever ORE=1 or RXNE=1 in the USART_SR register 
+	NVIC_EnableIRQ(USART1_IRQn);					// enable USART1 interrup
+	__enable_irq();
+	
+}
+ 
+void usart1_write(char ch)
+{
+	//while(!(USART1->SR & 0x0080)){}	//wait while TX buffer is empty
+	USART1->DR = ch & 0xFF;
+  while(!(USART1->SR & 0x0040)){}		// wait until transmission is completed
+		
+}
+
+void usart1_send_string(char * str)
+{
+	unsigned int i = 0;
+	while(str[i] != '\0')
+	{
+		usart1_write(str[i]);
+		i++;
+	}
+}
+
+char usart1_read(void)
+{
+	while(!((USART1->SR & 0x00000020) == 0x00000020)){}	// wait until received data is ready to be read
+		
+	return (char)USART1->DR;		
+}
+
+ void gpio_init(void)
+ {
+	 RCC->AHBENR |= 0x00000002;			// IO port B clock enabled (bit 1) --> All GPIOs are connected to AHB bus, so the relevant register is RCC_AHBENR 
+	 GPIOB->MODER |= 0x00005000;		// As long as reset value is 0, we just need to write 01 to the relavant section to configure PB6 and PB7 as output 
+	 
+	 RCC->AHBENR |= 0x00000001;										// IO port A clock enabled (bit 0) --> All GPIOs are connected to AHB bus, so the relevant register is RCC_AHBENR 
+	 GPIOA->MODER &= (unsigned int)~0x00000003;		// set PA0 as input (0b00)	
+ }
+ 
+ void led_blue(int status)
+ {
+	 if(status)										// Turn on LED blue PB6
+	 {
+		 GPIOB->BSRR = 0x00000040;				// Atomic sets the corresponding ODRB bit 
+	   //GPIOB->ODR |= 0x00000040;			// Sets ODR6 bit	
+	 }
+	 else													// Turn off LED blue PB6
+	 {
+		 GPIOB->BSRR = 0x00400000;				// Atomic resets the corresponding ODRB bit
+		 //GPIOB->ODR &= ~0x00000040;			// Resets ODR6 bit
+	 }
+ }
+ 
+ void led_green(int status)
+ {
+	 if(status)										// Turn on LED green PB7
+	 {
+		 GPIOB->BSRR = 0x00000080;				// Atomic sets the corresponding ODRB bit 
+	   //GPIOB->ODR |= 0x00000080;			// Sets ODR7 bit	
+	 }
+	 else													// Turn off LED green PB7
+	 {
+		 GPIOB->BSRR = 0x00800000;				// Atomic resets the corresponding ODRB bit
+		 //GPIOB->ODR &= ~0x00000080;			// Resets ODR7 bit
+	 }
+ }
+
+ void set_sysclk_to_hsi(void)
+ {
+	 RCC->CR &= 0x8EFAFEFE;		// clear all rw bits
+	 RCC->CFGR &= 0x8802C00C;	// clear all rw bits
+	 
+	 RCC->CR |= 0x00000001;		// HSI oscillator ON
+	 RCC->CFGR |= 0x00000001;	// HSI oscillator used as system clock
+	 
+	 while(!((RCC->CR & 0x00000002) == 0x00000002)){}	// waitn until HSI oscillator is ready
+	 while(!((RCC->CFGR & 0x0000000C) == 0x04)){}			// wait until HSI is used as system clock 	 
+	 
+	 SystemCoreClockUpdate();
+ }
+ 
+ 
+ // that will change the speed based on the optimization settings
+void delay_ms(int delay)
+{
+	volatile int i;
+	int j;
+	for(j=delay;j>0;j--)
+	{
+		for(i=0;i<500;i++);
+	}
+}
+
+ 
